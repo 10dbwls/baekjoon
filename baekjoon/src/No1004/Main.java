3aee65b041692f79f075fd8017e057234f6fd781
10dbwls <69668951+10dbwls@users.noreply.github.com>
2022-08-05 16:59:32 +0900

    no 1004

---
+++ baekjoon/src/No1004/Main.java
@@ -0,0 +1,28 @@
+package No1004;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		int case_num = scanner.nextInt();
+		
+		for (int i = 0; i < case_num; i++) {
+			int x1 = scanner.nextInt();
+			int y1 = scanner.nextInt();
+			int x2 = scanner.nextInt();
+			int y2 = scanner.nextInt();
+			
+			int cnt = scanner.nextInt();
+			
+			for (int j = 0; j < cnt; j++) {
+				int cx = scanner.nextInt();
+				int cy = scanner.nextInt();
+				int r = scanner.nextInt();
+				
+				
+			}
+		}
+	}
+}
